8224384a7b992fa36d944722710379e432c6068e
Tiger Wang <tigerwang@outlook.com>
2022-05-20 11:14:05 -0400

    wip

--- eth-contracts/mint.js
+++ eth-contracts/mint.js
@@ -1,114 +1,39 @@
+const fs = require('fs');
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const web3 = require("web3");
-const MNEMONIC = process.env.MNEMONIC;
-const NODE_API_KEY = process.env.INFURA_KEY || process.env.ALCHEMY_KEY;
-const isInfura = !!process.env.INFURA_KEY;
-const FACTORY_CONTRACT_ADDRESS = process.env.FACTORY_CONTRACT_ADDRESS;
-const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS;
-const OWNER_ADDRESS = process.env.OWNER_ADDRESS;
-const NETWORK = process.env.NETWORK;
-const NUM_CREATURES = 12;
-const NUM_LOOTBOXES = 4;
-const DEFAULT_OPTION_ID = 0;
-const LOOTBOX_OPTION_ID = 2;
 
-if (!MNEMONIC || !NODE_API_KEY || !OWNER_ADDRESS || !NETWORK) {
-  console.error(
-    "Please set a mnemonic, Alchemy/Infura key, owner, network, and contract address."
-  );
-  return;
-}
-
-const NFT_ABI = [
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "_to",
-        type: "address",
-      },
-    ],
-    name: "mintTo",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-];
-
-const FACTORY_ABI = [
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "_optionId",
-        type: "uint256",
-      },
-      {
-        name: "_toAddress",
-        type: "address",
-      },
-    ],
-    name: "mint",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-];
+const INFURA_KEY = fs.readFileSync(".key").toString().trim();
+const MNEMONIC = fs.readFileSync(".secret").toString().trim();
+const CONTRACT_ADDRESS = "0x720484c6c3f25Dfc49de0fD7cc720B88D3103fA0";
+const OWNER_ADDRESS = "0x7be53c45fA6567e26Ea9cC1E2636AB910bb75511";
+const NETWORK = "rinkeby";
+const contract = require('./build/contracts/SolnSquareVerifier.json');
+const ABI = contract.abi;
 
 async function main() {
-  const network =
-    NETWORK === "mainnet" || NETWORK === "live" ? "mainnet" : "rinkeby";
-  const provider = new HDWalletProvider(
-    MNEMONIC,
-    isInfura
-      ? "https://" + network + ".infura.io/v3/" + NODE_API_KEY
-      : "https://eth-" + network + ".alchemyapi.io/v2/" + NODE_API_KEY
-  );
-  const web3Instance = new web3(provider);
+  console.log("start main");
+  const provider = new HDWalletProvider(MNEMONIC, `https://${NETWORK}.infura.io/v3/${INFURA_KEY}`)
+  const web3Instance = new web3(provider)
+  console.log("finished set provider ");
+  if (CONTRACT_ADDRESS) {
+    //check abi and address
+    console.log("CONTRACT_ADDRESS :" + CONTRACT_ADDRESS);
+    const token = new web3Instance.eth.Contract(ABI, CONTRACT_ADDRESS, { gasLimit: "4500000" })
+    const zokratesProof = require("../zokrates/code/square/proof.json");
 
-  if (FACTORY_CONTRACT_ADDRESS) {
-    const factoryContract = new web3Instance.eth.Contract(
-      FACTORY_ABI,
-      FACTORY_CONTRACT_ADDRESS,
-      { gasLimit: "1000000" }
-    );
+    console.log("zokratesProof :" + JSON.stringify(zokratesProof.proof.a));
 
-    // Creatures issued directly to the owner.
-    for (var i = 0; i < NUM_CREATURES; i++) {
-      const result = await factoryContract.methods
-        .mint(DEFAULT_OPTION_ID, OWNER_ADDRESS)
+    try {
+      const tokenId = 1;
+      console.log("OWNER_ADDRESS " + OWNER_ADDRESS + "\n");
+      let tx2 = await token.methods
+        .mint(OWNER_ADDRESS, 1)
         .send({ from: OWNER_ADDRESS });
-      console.log("Minted creature. Transaction: " + result.transactionHash);
-    }
 
-    // Lootboxes issued directly to the owner.
-    for (var i = 0; i < NUM_LOOTBOXES; i++) {
-      const result = await factoryContract.methods
-        .mint(LOOTBOX_OPTION_ID, OWNER_ADDRESS)
-        .send({ from: OWNER_ADDRESS });
-      console.log("Minted lootbox. Transaction: " + result.transactionHash);
+      console.log("Minted item. Transaction: " + tx2.transactionHash);
+    } catch (e) {
+      console.log("error into minted function " + e);
     }
-  } else if (NFT_CONTRACT_ADDRESS) {
-    const nftContract = new web3Instance.eth.Contract(
-      NFT_ABI,
-      NFT_CONTRACT_ADDRESS,
-      { gasLimit: "1000000" }
-    );
-
-    // Creatures issued directly to the owner.
-    for (var i = 0; i < NUM_CREATURES; i++) {
-      const result = await nftContract.methods
-        .mintTo(OWNER_ADDRESS)
-        .send({ from: OWNER_ADDRESS });
-      console.log("Minted creature. Transaction: " + result.transactionHash);
-    }
-  } else {
-    console.error(
-      "Add NFT_CONTRACT_ADDRESS or FACTORY_CONTRACT_ADDRESS to the environment variables"
-    );
   }
 }
-
-main();
+main()
